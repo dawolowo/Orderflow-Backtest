7f0a8a0c328eb3f692701feba37ab891327457a6
David <113637215+dawolowo@users.noreply.github.com>
2024-10-12 18:31:38 +0100

    Update level_info.h

--- implementation/level_info.h
+++ implementation/level_info.h
@@ -8,30 +8,30 @@ This file contains code that describes the market level.
 
 /*A structure that contains information about the bid and ask volumes traded at a price*/
 struct Level{
-    Price price_;
-    Quantity bids_ = 0;
-    Quantity asks_ = 0;
+    Price price;
+    Quantity bids = 0;
+    Quantity asks = 0;
     // Checks for buy imbalance
     bool buy_imbalance(double ratio) const {
-        return bids_ > ratio * asks_;
+        return bids > ratio * asks;
     }
     // Checks for sell imbalance
     bool sell_imbalance(double ratio) const{
-        return asks_ > ratio * bids_;
+        return asks > ratio * bids;
     }
     
     Level operator+(const Level &operand){
-        if (price_ != operand.price_) throw std::logic_error("operator+ error : Cannot add levels with different price\n");
-        return Level(price_, bids_+operand.bids_, asks_+operand.asks_);
+        if (price != operand.price) throw std::logic_error("operator+ error : Cannot add levels with different price\n");
+        return Level(price, bids+operand.bids, asks+operand.asks);
     }
     
     friend std::ostream &operator<<(std::ostream &out, Level &obj){
-        out << obj.price_ << " " << obj.bids_ << " " << obj.asks_;
+        out << obj.price << " " << obj.bids << " " << obj.asks;
         return out;
     }
     
     friend std::istream &operator>>(std::istream &in, Level &obj){
-        in >> obj.price_ >> obj.bids_ >> obj.asks_;
+        in >> obj.price >> obj.bids >> obj.asks;
         return in;
     }
 
