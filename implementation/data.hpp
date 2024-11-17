ec9929c2861eeb02008fc351028b082431259914
David <113637215+dawolowo@users.noreply.github.com>
2024-11-17 13:22:02 +0100

    Update data.hpp

--- implementation/data.hpp
+++ implementation/data.hpp
@@ -8,10 +8,10 @@
 
 namespace data{
     std::fstream file_in;
-
+   
     inline void open_file(const char *path){
         file_in.open(path, std::ios::in);
-        if (!file_in) throw std::logic_error("cause = data::open_file() : File was not opened\n");
+        if (!file_in.is_open()) throw std::logic_error("cause = data::open_file() : File was not opened\n");
     }
 
     inline void close_file(){ 
@@ -22,16 +22,16 @@ namespace data{
     @param ncol number of columns to read
     @param row vector that will be filled with the read row of the csv file. Empty vector should be passed
     */
-    inline void stream_file(const size_t ncol, std::vector<std::string> &row){
+    inline void stream_file(const size_t &ncol, std::vector<std::string> &row){
         char x;
-        std::string temp;
+        size_t n = 0;
+        row[0] = "";
         while (file_in.get(x)){
             if (x == '\n') break;
-            if (x == ',' && row.size() < ncol){
-                row.push_back(temp);
-                temp = "";
+            if (x == ',' && n+1 < ncol){
+                row[++n] = "";
             }
-            else temp += x;
+            else if (n < ncol) row[n] += x;
         }
     }
     
@@ -43,9 +43,12 @@ namespace data{
     inline void stream_file(const std::vector<const char *> &cols, std::unordered_map<const char *, std::string> &row){
         size_t n = 0;
         char x;
+        row[cols[n]] = "";
         while (file_in.get(x)){
             if (x == '\n') break;
-            if (x == ',' ) n++;
+            if (x == ',' ) {
+                row[cols[++n]] = "";
+            }
             else if (n < cols.size()){
                 row[cols[n]] += x;
             }
