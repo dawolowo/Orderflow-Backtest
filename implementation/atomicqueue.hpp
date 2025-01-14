ef9e4f2b1bd9b8760126987be7b4c7e32cfbf006
David <113637215+dawolowo@users.noreply.github.com>
2025-01-14 12:18:38 +0100

    Update atomicqueue.hpp

--- implementation/atomicqueue.hpp
+++ implementation/atomicqueue.hpp
@@ -8,46 +8,46 @@
 */
 template <typename Type, typename Sequence = std::deque<Type>>
 class AtomicQueue {
+
 private:
-    std::queue<Type> _data;
+    std::queue<Type, Sequence> _data;
     std::mutex _mut_lock;
+    
 public:
     AtomicQueue() = default;
 
     ~AtomicQueue(){_mut_lock.unlock();}
 
     //Returns the number of elements in the %AtomicQueue.
-    size_t size(){
+    size_t size() noexcept{
         std::lock_guard<std::mutex> lock(_mut_lock);
         return _data.size();
     }
 
     //Returns true if the %AtomicQueue is empty.
-    bool empty(){
+    bool empty() noexcept {
         std::lock_guard<std::mutex> lock(_mut_lock);
         return _data.empty();
     }
 
     /*Add data to the end of the %queue.
-
-    @param __x – Data to be added.
+    @param __x Data to be added.
     
     This is a typical %AtomicQueue operation. The function creates an element at the end of the %queue and assigns the given data to it.
     The time complexity of the operation depends on the underlying sequence.*/
-    void push(Type &__x){
+    void push(Type &__x) noexcept{
         std::lock_guard<std::mutex> lock(_mut_lock);
         _data.push(__x);
     }
 
     /*Add data to the end of the %queue.
+    @param __x  Data to be added.
 
-    @param __x – Data to be added.
-    
     This is a typical %AtomicQueue operation. The function creates an element at the end of the %queue and assigns the given data to it.
     The time complexity of the operation depends on the underlying sequence.*/
-    void push(Type &&__x){
+    void push(Type &&__x)noexcept{
         std::lock_guard<std::mutex> lock(_mut_lock);
-        _data.push(__x);
+        _data.push(std::move(__x));
     }
 
     /*Removes first element.
