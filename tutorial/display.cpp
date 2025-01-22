e0c4ad5ba3818d4c3eb96a204f4ac69173af6f6b
David <113637215+dawolowo@users.noreply.github.com>
2025-01-22 22:10:01 +0100

    Create display.cpp

---
+++ tutorial/display.cpp
@@ -0,0 +1,28 @@
+#include "./implementation/chart.hpp"
+
+/*
+Tutorial: How to display the footprint, delta, bar of the candle
+*/
+
+using namespace std;
+
+int main(){
+    const char *file_path = ".txt"; //Location of the aggregated time and sales
+
+    Chart chart;
+    chart.load(file_path);
+    cout << "Number of candles: " << chart.size() << '\n'
+    << "Enter your desired index from 0 to " << chart.size()-1 << ". Enter -1 to exit program\n";
+
+    int response = 0;
+    
+    while (response != -1){
+        cout << "index: ";
+        cin >> response;
+        chart[response].print_fp();
+        // chart[response].print_bar();
+        // chart[response].print_delta();
+    }
+
+    return 0;
+}
