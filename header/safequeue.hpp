53df70f692a25d099566400978502f2729d77568
David <113637215+dawolowo@users.noreply.github.com>
2025-03-10 18:07:13 +0100

    Update and rename atomicqueue.hpp to safequeue.hpp
    Changed class name from AtomicQueue to SafeQueue. 
    Added volatile member variable to reduce amount of lock holding

--- header/safequeue.hpp
+++ header/safequeue.hpp
@@ -7,61 +7,66 @@
 @tparam Sequence  Type of underlying sequence, defaults to deque<Type>.
 */
 template <typename Type, typename Sequence = std::deque<Type>>
-class AtomicQueue {
+class SafeQueue {
 
 private:
     std::queue<Type, Sequence> _data;
     std::mutex _mut_lock;
-    
+    volatile bool _is_empty = true;
+    volatile size_t _len = 0;
+
 public:
-    AtomicQueue() = default;
+    SafeQueue() = default;
 
-    ~AtomicQueue(){_mut_lock.unlock();}
+    ~SafeQueue(){_mut_lock.unlock();}
 
-    //Returns the number of elements in the %AtomicQueue.
+    //Returns the number of elements in the %SafeQueue.
     size_t size() noexcept{
-        std::lock_guard<std::mutex> lock(_mut_lock);
-        return _data.size();
+        return _len;
     }
 
-    //Returns true if the %AtomicQueue is empty.
+    //Returns true if the %SafeQueue is empty.
     bool empty() noexcept {
-        std::lock_guard<std::mutex> lock(_mut_lock);
-        return _data.empty();
+        return _is_empty;
     }
 
     /*Add data to the end of the %queue.
     @param __x Data to be added.
     
-    This is a typical %AtomicQueue operation. The function creates an element at the end of the %queue and assigns the given data to it.
+    This is a typical %SafeQueue operation. The function creates an element at the end of the %queue and assigns the given data to it.
     The time complexity of the operation depends on the underlying sequence.*/
     void push(Type &__x) noexcept{
         std::lock_guard<std::mutex> lock(_mut_lock);
         _data.push(__x);
+        _len += 1;
+        _is_empty = false;
     }
 
     /*Add data to the end of the %queue.
     @param __x  Data to be added.
 
-    This is a typical %AtomicQueue operation. The function creates an element at the end of the %queue and assigns the given data to it.
+    This is a typical %SafeQueue operation. The function creates an element at the end of the %queue and assigns the given data to it.
     The time complexity of the operation depends on the underlying sequence.*/
     void push(Type &&__x)noexcept{
         std::lock_guard<std::mutex> lock(_mut_lock);
         _data.push(std::move(__x));
+        _len += 1;
+        _is_empty = false;
     }
 
     /*Removes first element.
-
-    This is a typical %AtomicQueue operation. It shrinks the %AtomicQueue by one. The time complexity of the operation depends on the
-     underlying sequence.
+    This is a typical %SafeQueue operation. It shrinks the %SafeQueue by one. The time complexity of the operation depends on the
+    underlying sequence.
 
     @note No data is returned, and if the first element's data is needed, it should be retrieved before pop() is called.*/
     void pop(){
         std::lock_guard<std::mutex> lock(_mut_lock);
         _data.pop();
+        _len -= 1;
+        _is_empty = _data.empty();
     }
 
-    /*Returns a read/write reference to the data at the first element of the %AtomicQueue.*/
+    /*Returns a read/write reference to the data at the first element of the %SafeQueue.*/
     Type &front(){
         std::lock_guard<std::mutex> lock(_mut_lock);
         return _data.front();
